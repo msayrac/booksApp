706ec4e3e6313a266088ebbc6a84970e6d669ec4
msayracode <msayracode@gmail.com>
2026-08-06 12:14:19 +0300

    filter options added to api and web layer

--- books/api/views.py
+++ books/api/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics
+from rest_framework import generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from books.models import Book,Category
 from .serializers import BookSerializer,CategorySerializer
 
@@ -7,6 +8,22 @@ from .serializers import BookSerializer,CategorySerializer
 class BookListCreateAPIView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+    # filtreleme motorlarını tanımlıyoruz
+    filter_backends = [
+        DjangoFilterBackend, # filtremele / eşleşme
+        filters.SearchFilter, # Search Bar
+        filters.OrderingFilter, # Sıralama price, date
+    ]
+
+    # birebir filtreleme yapılacak alanlar
+    filterset_fields = ['category']
+
+    # metin arama yapılacak (title, author)
+    search_fields = ['title', 'author']
+
+    ordering_fields = ['id','price', 'publish_date']
+    ordering = ['-id']
 
 # a book retrieve (GET) update (PUT) delete (DELETE)
 class BookDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
--- books/views.py
+++ books/views.py
@@ -6,8 +6,18 @@ from .models import Book
 def book_list_web(request):
     books = Book.objects.all()
 
+    # aram kutuusndan gelen kelimeyi alıyor
+
+    q = request.GET.get('q')
+
+    if q:
+        # baslıkta veya yazar kısmında aranan kelimeler filtreler
+
+        books = books.filter(title__icontains=q) | books.filter(author__icontains=q) 
+
     context ={
-        'books':books
+        'books':books,
+        'arama_kelimesi':q or ''
     }
 
     return render(request, 'books/book_list.html',context)
