ae5a00cdc816e4906b209ad8beb804dcbd2d0635
msayracode <msayracode@gmail.com>
2026-08-10 14:59:35 +0300

    category_detail is simplified in the serializer

--- books/api/serializers.py
+++ books/api/serializers.py
@@ -2,14 +2,22 @@ from rest_framework import serializers
 from books.models import Book, Category
 
 
-class CategorySerializer(serializers.ModelSerializer):
+class CategorySimpleSerializer(serializers.ModelSerializer):
+    # books = serializers.StringRelatedField(many=True, read_only=True)
     class Meta:
         model = Category
         fields = ['id','name','slug']
 
 class BookSerializer(serializers.ModelSerializer):
-    category_name = serializers.CharField(source='category.name',read_only=True)
+    category_detail = CategorySimpleSerializer(source='category', read_only =True)
+    # category_name = serializers.CharField(source='category.name',read_only=True)
 
     class Meta:
         model = Book
-        fields = ['id','title','author','description','price','stock','publish_date','category','category_name']
+        fields = ['id','title','author','description','price','stock','publish_date','category','category_detail']
+
+class CategorySerializer(serializers.ModelSerializer):
+    books = serializers.StringRelatedField(many=True, read_only=True)
+    class Meta:
+        model = Category
+        fields = ['id','name','slug','books']
